52e7f0da66ce6bc91b21eabacef034344884e9b7
Wesley Raul <wesleyrau56@gmail.com>
2024-10-09 19:38:48 -0300

    feat: connect list pokemons with backend

--- frontend/src/app/api/captured-pokemons/route.ts
+++ frontend/src/app/api/captured-pokemons/route.ts
@@ -2,59 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const capturedPokemons = [
-      {
-        name: "pikachu",
-        types: [{ type: { name: "electric" } }],
-        sprites: {
-          front_default:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
-        },
-        weight: 3,
-        height: 4,
-      },
-      {
-        name: "charmander",
-        types: [{ type: { name: "fire" } }],
-        sprites: {
-          front_default:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-        },
-        weight: 75,
-        height: 6,
-      },
-      {
-        name: "charmander",
-        types: [{ type: { name: "fire" } }],
-        sprites: {
-          front_default:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-        },
-        weight: 75,
-        height: 6,
-      },
-      {
-        name: "charmander",
-        types: [{ type: { name: "fire" } }],
-        sprites: {
-          front_default:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-        },
-        weight: 75,
-        height: 6,
-      },
-      {
-        name: "charmander",
-        types: [{ type: { name: "fire" } }],
-        sprites: {
-          front_default:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-        },
-        weight: 75,
-        height: 6,
-      },
-    ];
-    return NextResponse.json(capturedPokemons);
+    const response = await fetch("http://localhost:3001/pokemon/");
+    if (!response.ok) {
+      throw new Error("Failed to fetch pokemons");
+    }
+    const capturedPokemons = await response.json();
+    const pokemons = capturedPokemons.data;
+    return NextResponse.json(pokemons);
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch captured pokemons" });
   }
