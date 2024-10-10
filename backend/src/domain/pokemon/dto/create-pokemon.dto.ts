d78f1896658fef4606aa298c5ab2562265d66ac9
Wesley Raul <wesleyrau56@gmail.com>
2024-10-09 22:04:27 -0300

    feat: add swagger doc

--- backend/src/domain/pokemon/dto/create-pokemon.dto.ts
+++ backend/src/domain/pokemon/dto/create-pokemon.dto.ts
@@ -1,15 +1,41 @@
 import { IsArray, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePokemonDto {
-  @IsString({ message: 'Sprite must be a string' })
+  @ApiProperty({
+    description: 'The URL of the Pokémon sprite image.',
+    example: 'https://example.com/pikachu.png',
+  })
+  @IsString({ message: 'The sprite URL must be a valid string.' })
   sprite: string;
 
-  @IsString({ message: 'Name must be a string' })
+  @ApiProperty({
+    description: 'The name of the Pokémon.',
+    example: 'Pikachu',
+  })
+  @IsString({ message: 'The Pokémon name must be a valid string.' })
   name: string;
 
-  @IsArray()
-  types: [string];
+  @ApiProperty({
+    description: 'An array of types associated with the Pokémon.',
+    example: ['Electric'],
+    isArray: true,
+  })
+  @IsArray({ message: 'Types must be an array of strings.' })
+  @IsString({ each: true, message: 'Each type must be a valid string.' })
+  types: string[];
 
+  @ApiProperty({
+    description: 'The height of the Pokémon in decimeters.',
+    example: 4,
+    type: String,
+  })
   height: string;
+
+  @ApiProperty({
+    description: 'The weight of the Pokémon.',
+    example: 60,
+    type: String,
+  })
   weight: string;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (!(process.env.NODE_ENV == 'prod')) {
+    const config = new DocumentBuilder()
+      .setTitle('Pokemon')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   app.useGlobalPipes(new ValidationPipe({}));
   await app.listen(process.env.PORT || 3001);
 }
